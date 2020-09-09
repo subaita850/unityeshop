4345a380ce5e6110ed7f469e08aff2b33b306394
subaita850 <71041812+subaita850@users.noreply.github.com>
2020-09-10 00:37:51 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+
+alert("hello");
+
+
+
+
+
+	 var productimg = document.getElementById('productimg');
+	 var smallimg = document.getElementsByClassName("small-img");
+	 
+	 smallimg[0].onclick = function()
+	   {
+		 productimg.src = smallimg[0].src
+	   }
+	   
+	   smallimg[1].onclick = function()
+	   {
+		 productimg.src = smallimg[1].src
+	   }
+	   
+	   smallimg[2].onclick = function()
+	   {
+		 productimg.src = smallimg[2].src
+	   }
+	   
+	   smallimg[3].onclick = function()
+	   {
+		 productimg.src = smallimg[3].src
+	   }
+	   
+	   smallimg[4].onclick = function()
+	   {
+		 productimg.src = smallimg[4].src
+	   }
+	   
+	   
+	   
+	   
+	   
